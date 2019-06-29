90aaa0b9aedf899c446281733f949c7a6bbf42b8
ycarowr <ywrycaro@gmail.com>
2019-06-29 19:55:25 +0300

    Add local data storage

---
+++ LocalData/Editor/LocalDataTest.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class LocalDataTest : MonoBehaviour
+{
+    [TearDown]
+    public void TearDown()
+    {
+        LocalData.DeleteAll();
+    }
+    
+    //------------------------------------------------------------------------------------------------------------------
+    
+    [Test]
+    public void StoreDataTest()
+    {
+        const string id = "StoreDataTest";
+        //clean data
+        var testData = new TestLocalData();
+
+        //new data
+        var newInt = int.MaxValue;
+        var newBool = true;
+        var newFloat = 123.123141f;
+        var newString = "ycaro";
+        
+        //change test data
+        testData.TestInt = newInt;
+        testData.TestBool = newBool;
+        testData.TestNestedData = new TestLocalData.NestedLocalData()
+        {
+            TestFloat = newFloat,
+            TestString = newString
+        };
+        
+        
+        //store the data locally
+        LocalData.StoreData(testData, id);
+            
+        //get the stored data
+        var retrievedData = LocalData.LoadData<TestLocalData>(id);
+        
+        //compare data
+        Assert.True(testData.IsEqual(retrievedData));
+    }
+
+    [Test]
+    public void RemoveDataTest()
+    {
+        const string id = "RemoveDataTest";
+        var testData = new TestLocalData();
+        LocalData.StoreData(testData, id);
+        LocalData.DeleteData(id);
+        Assert.False(LocalData.HasData(id));
+    }
+    
+    [Test]
+    public void HasDataTest()
+    {
+        const string id = "HasDataTest";
+        var testData = new TestLocalData();
+        
+        //yes
+        LocalData.StoreData(testData, id);
+        Assert.True(LocalData.HasData(id));
+        
+        //no
+        LocalData.DeleteData(id);
+        Assert.False(LocalData.HasData(id));
+    }
+    
+    //------------------------------------------------------------------------------------------------------------------
+
+    [Serializable]
+    public class TestLocalData
+    {
+        public int TestInt;
+        public bool TestBool;
+        public NestedLocalData TestNestedData;
+
+        [Serializable]
+        public class NestedLocalData
+        {
+            public float TestFloat;
+            public string TestString;
+
+            public bool IsEqual(NestedLocalData other)
+            {
+                return TestFloat == other.TestFloat && TestString == other.TestString;
+            }
+        }
+
+        public bool IsEqual(TestLocalData other)
+        {
+            return TestBool == other.TestBool
+                   && TestInt == other.TestInt
+                   && TestNestedData.IsEqual(other.TestNestedData);
+        }
+    }
+}
---
+++ LocalData/LocalData.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using FullSerializer;
+using UnityEngine;
+
+/// <summary>
+///     Wraps up serialization functionalities and local data storage.
+/// </summary>
+public class LocalData
+{
+    /// <summary>
+    ///     ID of the local serialized data.
+    /// </summary>
+    private const string localDataId = "LocalData";
+
+    /// <summary>
+    ///     Register for all the local data.
+    /// </summary>
+    [fsProperty]
+    private static Dictionary<string, string> Register { get; } = LoadAllData();
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /// <summary>
+    ///     Store the data locally. Makes use of PlayerPrefs.
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="id"></param>
+    /// <typeparam name="T"></typeparam>
+    public static void StoreData<T>(T data, string id) where T : class
+    {
+        var sData = Serialize(data);
+        Register[id] = sData;
+        StoreAllData();
+        PrintLocalData();
+    }
+
+    /// <summary>
+    ///     Retrieves the data from the id.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static T LoadData<T>(string id) where T : class
+    {
+        var data = string.Empty;
+        Register?.TryGetValue(id, out data);
+        return Deserialize<T>(data);
+    }
+
+    /// <summary>
+    ///     Remove the data from the id.
+    /// </summary>
+    /// <param name="id"></param>
+    public static void DeleteData(string id)
+    {
+        Register.Remove(id);
+        StoreAllData();
+    }
+
+    /// <summary>
+    ///     Check if id is present in the current data.
+    /// </summary>
+    /// <param name="id"></param>
+    public static bool HasData(string id)
+    {
+        return Register.ContainsKey(id);
+    }
+
+    /// <summary>
+    ///     Remove all the data from the app.
+    /// </summary>
+    public static void DeleteAll()
+    {
+        Register.Clear();
+        PlayerPrefs.DeleteAll();
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /// <summary>
+    ///     Deserialize the data from json format.
+    /// </summary>
+    /// <param name="json"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static T Deserialize<T>(string json) where T : class
+    {
+        //using Unity built in solution
+        //return JsonUtility.FromJson<T>(json);
+        
+        //-------------------------------------------------------
+        
+        //using Fullserializer solution
+        var type = typeof(T);
+        return FullSerializerWrapper.Deserialize(type, json) as T;
+    }
+
+    /// <summary>
+    ///     Serialize the data using json format.
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="isPretty"></param>
+    /// <returns></returns>
+    public static string Serialize(object data, bool isPretty = false)
+    {
+        //using Unity built in solution
+        //return JsonUtility.ToJson(data, isPretty);
+        
+        //-------------------------------------------------------
+        
+        //using Fullserializer solution
+        var type = data.GetType();
+        return FullSerializerWrapper.Serialize(type, data);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    private static Dictionary<string, string> LoadAllData()
+    {
+        var localData = PlayerPrefs.GetString(localDataId);
+        return string.IsNullOrEmpty(localData)
+            ? new Dictionary<string, string>()
+            : JsonUtility.FromJson<Dictionary<string, string>>(localData);
+    }
+
+    private static void StoreAllData()
+    {
+        var allData = Serialize(Register);
+        PlayerPrefs.SetString(localDataId, allData);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /// <summary>
+    ///     Prints with Debug.Log all the local stored data and its IDs.
+    /// </summary>
+    public static void PrintLocalData()
+    {
+        SaveCopyAtAssets();
+        Debug.Log(Serialize(Register, true));
+    }
+
+    public static void SaveCopyAtAssets()
+    {
+        var text = Serialize(Register, true);
+        File.WriteAllText("Assets/Snapshot.json", text);
+    }
+
+    /// <summary>
+    ///     Wrapper for FullSerializer. Ref: https://github.com/jacobdufault/fullserializer.
+    /// </summary>
+    private static class FullSerializerWrapper
+    {
+        private static readonly fsSerializer _serializer = new fsSerializer();
+
+        public static string Serialize(Type type, object value)
+        {
+            // serialize the data
+            fsData data;
+            _serializer.TrySerialize(type, value, out data).AssertSuccessWithoutWarnings();
+
+            // emit the data via JSON
+            return fsJsonPrinter.CompressedJson(data);
+        }
+
+        public static object Deserialize(Type type, string serializedState)
+        {
+            // step 1: parse the JSON data
+            var data = fsJsonParser.Parse(serializedState);
+
+            // step 2: deserialize the data
+            object deserialized = null;
+            _serializer.TryDeserialize(data, type, ref deserialized).AssertSuccessWithoutWarnings();
+
+            return deserialized;
+        }
+    }
+}
