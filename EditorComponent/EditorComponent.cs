9f1d14a57a6315a83bc809efdffb06bdc33e9291
ycarowr <ywrycaro@gmail.com>
2019-08-03 16:17:06 +0300

    Add editor component

---
+++ EditorComponent/EditorComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tools
+{
+    public class EditorComponent : MonoBehaviour
+    {
+        protected void OnEnable()
+        {
+            if (!Application.isEditor)
+                Destroy(this);
+        }
+    }
+}
