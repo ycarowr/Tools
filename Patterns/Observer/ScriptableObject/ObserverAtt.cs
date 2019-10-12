45e08781eaafb448c3989a4c113df9a948d76007
ycarowr <ywrycaro@gmail.com>
2019-10-12 17:23:08 +0300

    Update Observer

--- Patterns/Observer/ScriptableObject/ObserverAtt.cs
+++ Patterns/Observer/ScriptableObject/ObserverAtt.cs
@@ -6,49 +6,46 @@ namespace Tools.Patterns.Observer
 {
     public class ObserverAtt<T> : ScriptableObject where T : Attribute
     {
-        readonly Dictionary<Type, List<IListener>> register = new Dictionary<Type, List<IListener>>();
+        readonly Dictionary<Type, List<IListener>> _register = new Dictionary<Type, List<IListener>>();
 
-        public virtual void AddListener(IListener listener)
+        public void AddListener(IListener listener)
         {
             if (listener == null)
                 throw new ArgumentNullException("Can't register Null as a Listener");
 
             var type = listener.GetType();
-            Debug.Log("Type: "+ type);
             var interfaces = type.GetInterfaces();
             
             foreach (var i in interfaces)
             {
-                var customAttributes = i.GetCustomAttributes(true);    
-                for (var att = 0; att < customAttributes.Length; att++)
+                var customAttributes = i.GetCustomAttributes(true);
+                foreach (var subject in customAttributes)
                 {
-                    var subject = customAttributes[att];               
-                    Debug.Log(subject);
                     if(subject is T)
                         CreateAndAdd(i, listener);
-                }    
+                }
             }
         }
 
-        public virtual void RemoveListener(IListener listener)
+        public void RemoveListener(IListener listener)
         {
-            foreach (var pair in register)
+            foreach (var pair in _register)
                 pair.Value.Remove(listener);
         }
 
-        public virtual void RemoveSubject(Type subject)
+        public void RemoveSubject(Type subject)
         {
-            if (register.ContainsKey(subject))
-                register.Remove(subject);
+            if (_register.ContainsKey(subject))
+                _register.Remove(subject);
         }
 
         public void Notify<T1>(Action<T1> subject) where T1 : class
         {
             var subjectType = typeof(T1);
-            var isSubject = register.ContainsKey(subjectType);
+            var isSubject = _register.ContainsKey(subjectType);
             if (!isSubject)
                 return;
-            var listeners = register[subjectType];
+            var listeners = _register[subjectType];
             if (listeners.Count == 0) return;
 
             for (var i = 0; i < listeners.Count; i++)
@@ -59,13 +56,12 @@ namespace Tools.Patterns.Observer
             }
         }
 
-        protected void CreateAndAdd(Type subject, IListener listener)
+        void CreateAndAdd(Type subject, IListener listener)
         {
-            Debug.Log("Create and add: "+ subject);
-            if (register.ContainsKey(subject))
-                register[subject].Add(listener);
+            if (_register.ContainsKey(subject))
+                _register[subject].Add(listener);
             else
-                register.Add(subject, new List<IListener> {listener});
+                _register.Add(subject, new List<IListener> {listener});
         }
     }
 }
